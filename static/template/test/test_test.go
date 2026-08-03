8d3977cb1128e4b9500f2cc6bc5a37b588288c14
Adam Bibby <adam@bibby.io>
2026-08-02 21:23:45 -0400

    remove file level coverage requirement

--- static/template/test/test_test.go
+++ static/template/test/test_test.go
@@ -1,22 +1,30 @@
-package test_test
+package test
 
 import (
 	"testing"
 
-	"github.com/abibby/salusa/static/template/test"
 	"github.com/jmoiron/sqlx"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestRun(t *testing.T) {
-	test.Run(t, "runs with a transaction", func(t *testing.T, tx *sqlx.Tx) {
-		assert.NotNil(t, tx)
-	})
+func TestSetupTestDB(t *testing.T) {
+	db, err := setupTestDB("sqlite", ":memory:")
+	require.NoError(t, err)
+	require.NotNil(t, db)
+	require.NoError(t, db.Close())
+
+	_, err = setupTestDB("unknown-driver", ":memory:")
+	assert.Error(t, err)
+
+	_, err = setupTestDB("sqlite", "/nonexistent-dir/does-not-exist.sqlite")
+	assert.Error(t, err)
 }
 
-func TestHelpers(t *testing.T) {
-	require.NotNil(t, test.Run)
-	require.NotNil(t, test.RunBenchmark)
-	require.NotNil(t, test.Kernel)
+func TestRun(t *testing.T) {
+	ran := false
+	Run(t, "runner", func(t *testing.T, tx *sqlx.Tx) {
+		ran = true
+	})
+	assert.True(t, ran)
 }
